7b6ec7ffdf0caf306c1941bd7fd9f5420812fb87
Celso Fabri Junior <celso.fabri@live.com>
2021-05-27 15:26:33 -0300

    feat: create basic Header component

---
+++ src/components/Header/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="header">
+    <div class="wrap">
+      <h1 class="logo">Github Stars</h1>
+    </div>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'Header'
+  }
+</script>
+
+<style lang="scss">
+  @import './styles.scss';
+</style>
