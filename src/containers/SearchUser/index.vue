ff3e56ac7102391057eb64a197c77aa2d072b131
Celso Fabri Junior <celso.fabri@live.com>
2021-05-27 16:44:09 -0300

    feat: create SearchUser container component

---
+++ src/containers/SearchUser/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="search">
+    <div class="wrap">
+      <div class="search__container">
+        <form class="search__form">
+          <Input type="text" class="form__input" placeholder="Usuário do github"/>
+          <Button type="submit" class="btn" defaultValue="Buscar repositórios"/>
+        </form>
+        <div class="search__info">
+          <p>Insira o nome de usuário sem o @ no campo acima.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  import Input from '@/components/Form/Input';
+  import Button from '@/components/Form/Button';
+  import service from '@/services'
+
+  export default {
+    name: 'SearchUser',
+    components: {
+      Input,
+      Button
+    },
+    methods: {
+      getStarredRepositoriesByUser(username) {
+        service.getStarredRepositories(username).then((data) => {
+          console.log(data)
+        })
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import './styles.scss';
+</style>
