bc2108ed1bbd014798289555750ecf1dbc012ec6
Celso Fabri Junior <celso.fabri@live.com>
2021-05-27 16:44:48 -0300

    feat: create a basic footer

---
+++ src/components/Footer/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="footer">
+    <div class="wrap">
+      <p>Front-End Challenge: Github Stars developed by Celso Fabri, Diego Luz and Jean Masvi.</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: 'Footer'
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import './styles.scss';
+</style>
