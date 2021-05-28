308ecede9508670cf13144adb8f814db411642c2
Celso Fabri Junior <celso.fabri@live.com>
2021-05-28 18:25:46 -0300

    feat: create Search filter by tags component

---
+++ src/components/Search/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="search">
+    <Input 
+      :type="'text'"
+      :class="'form__input form__input--search'"
+      :placeholder="'Filtrar por tags'"
+    />
+    <p>sdfsdfsdf</p>
+  </div>
+</template>
+
+<script>
+  import Input from '@/components/Form/Input';
+
+  export default {
+    name: 'Filter',
+    components: {
+      Input
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import './styles.scss';
+</style>
