71403e95d9208890cd66715219826c9f405cf3f3
Celso Fabri Junior <celso.fabri@live.com>
2021-05-28 19:03:10 -0300

    feat: add rule for displays NotFound component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,23 @@
   <div>
     <BaseLoading v-show="isLoading" />
     <SearchUser 
-      v-if="!repositories.length"
+      v-if="!repositories.length && !isEmpty"
       @get-repos="getStarredRepositoriesByUser"
     />
     <RepositoryList
-      v-else
+      v-if="repositories.length && !isEmpty"
       :repositories="repositories"
+    />
+    <NotFound
+      v-if="!repositories.length && isEmpty"
+      @get-initial="getInitialScreen"
     />
   </div>
 </template>
 
 <script>
 import BaseLoading from '@/components/Loading';
+import NotFound from '@/components/NotFound';
 import RepositoryList from '@/containers/RepositoryList';
 import SearchUser from '@/containers/SearchUser';
 import service from '@/services'
@@ -22,23 +27,30 @@ export default {
   name: 'Home',
   components: {
     SearchUser,
-    RepositoryList,
-    BaseLoading
+    NotFound,
+    BaseLoading,
+    RepositoryList
   },
   data() {
     return {
       repositories: [],
-      isLoading: false
+      isLoading: false,
+      isEmpty: false
     };
   },
   methods: {
     getStarredRepositoriesByUser(username) {
       this.handleLoading();
       service.getStarredRepositories(username).then(({ data }) => {
-        console.log(data)
         this.repositories = data;
+        if(!data.length) {
+          this.isEmpty = true;
+        }
         this.handleLoading();
       });
+    },
+    getInitialScreen() {
+      this.isEmpty = false;
     },
     handleLoading() {
       this.isLoading = !this.isLoading;
