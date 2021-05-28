b2f3cdf4a15ff843c7c59de99c900245b7cd9b59
Masvi <jeanmasvi@gmail.com>
2021-05-28 11:43:35 -0300

    chore: change header and footer

--- src/components/Loading/index.vue
+++ src/components/Loading/index.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="fade">
-    <div class="base-loading">
+    <div class="loading">
       <img 
         src="@/assets/loading.svg"  
         alt="Loading"
@@ -17,16 +17,6 @@ export default {
 };
 </script>
 
-<style lang="scss">
-.base-loading {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 10;
-  width: 100vw;
-  height: 100vh;
-}
+<style lang="scss" scoped>
+  @import './styles.scss';
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,20 @@
 <template>
   <div>
-    <Header />
     <BaseLoading v-show="isLoading" />
     <SearchUser />
-    <Footer />
   </div>
 </template>
 
 <script>
-import Header from '@/components/Header';
-import BaseLoading from '@/components/BaseLoading';
+
+import BaseLoading from '@/components/Loading';
 import service from '@/services'
-import Footer from '@/components/Footer';
-import SearchUser from '@/containers/SearchUser';
+//import SearchUser from '@/containers/SearchUser';
 
 export default {
   name: 'Home',
   components: {
-    Header,
+
     BaseLoading
   },
   data() {
